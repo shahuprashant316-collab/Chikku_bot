d9768f488902b4045a7b775de8ef5e1c8f69dec7
shahuprashant316-collab <shahuprashant316@gmail.com>
2026-05-17 20:03:24 +0530

    Update gemini model name

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # =========================
 # START COMMAND
